b6a7e630c0b3f5b29a42836f5b1af38d7eeb9ecc
Andrei Iliescu <andrei.iliescu@stonesouptech.com>
2021-01-17 17:25:53 +0200

    ad coffee model

--- coffee/models.py
+++ coffee/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Cafea(models.Model):
+    title = models.CharField(max_length= 100)
+    description = models.TextField()
+    tip = models.CharField(max_length= 20)
+    image = models.FilePathField(path="/img")
     
