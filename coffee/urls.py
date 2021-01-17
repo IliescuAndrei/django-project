26566bc5fcd8d42b26b20e6efa6bb9e3f2898119
Andrei Iliescu <andrei.iliescu@stonesouptech.com>
2021-01-17 21:39:23 +0200

    add login register logout

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from coffee import views
+from coffee.views import LoginView, RegisterView, LogoutView
 
 urlpatterns = [
     path("", views.cafea_index, name="cafea_index"),
     path("<int:pk>/", views.cafea_detail, name="cafea_detail"),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from coffee.models import Cafea
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, ListView
+from django.views.generic import (
+    TemplateView, ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+
+
+from coffee.models import Cafea, User
 # Create your views here.
+
+
 
 def cafea_index(request):
     cafele = Cafea.objects.all()
@@ -16,3 +33,39 @@ def cafea_detail(request, pk):
     }
     return render(request, 'cafea_detail.html', context)
 
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+    def get_context_data(self):
+        form = AuthenticationForm()
+        return {'form': form}
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(username=data['username'],
+                                password=data['password'])
+            login(request, user)
+            return redirect(reverse_lazy('cafea_index'))
+        else:
+            return render(request, "login.html", {"form": form})
+
+
+class RegisterView(CreateView):
+    template_name= 'register.html'
+    form_class = UserCreationForm
+    model = User
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        user = User.objects.create_user(username=data['username'],
+                                        password=data['password1'])
+        return redirect('cafea_index')
+
+class LogoutView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect(reverse_lazy('cafea_index'))
